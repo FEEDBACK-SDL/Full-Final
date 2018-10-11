f5df137c939e841ed1f9169bb6f63528220879d9
Amey Deshpande <ameydeshpande30@gmail.com>
2018-10-11 20:02:27 +0530

    Login added with server and database

--- app/src/main/java/com/example/fedsev/feedback/API.java
+++ app/src/main/java/com/example/fedsev/feedback/API.java
@@ -1,10 +1,22 @@
 package com.example.fedsev.feedback;
 
+import org.androidannotations.annotations.rest.Post;
+
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface API {
-    static String BASE_URL = "http://192.168.43.154:3000/";
+    static String BASE_URL = "http://192.168.0.105:8000/";
     @GET("profile")
     Call<DataFromServer> getData();
+
+
+
+    @FormUrlEncoded
+    @POST("login")
+    Call<LoginData> loginD(@Field("uname")String uname,
+                 @Field("pass")String pass);
 }
