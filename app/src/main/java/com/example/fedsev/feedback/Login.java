76d4c38df6faa1d29c56ec4395ae922ce8444082
Amey Deshpande <ameydeshpande30@gmail.com>
2018-10-11 20:49:25 +0530

    Login added

---
+++ app/src/main/java/com/example/fedsev/feedback/Login.java
@@ -0,0 +1,20 @@
+package com.example.fedsev.feedback;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void goNext(View view) {
+        Intent i = new Intent(Login.this,MainActivity.class);
+        startActivity(i);
+    }
+}
